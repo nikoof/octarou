cf3a60ab71411371f1624b84373b026fcbd98269
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2023-09-30 13:23:03 +0300

    style: add pre-commit hooks for formatting

--- flake.nix
+++ flake.nix
@@ -3,42 +3,51 @@
     naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    pre-commit.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , utils
-    , naersk
-    ,
-    }:
-    utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      naersk-lib = pkgs.callPackage naersk { };
-      libPath = with pkgs; lib.makeLibraryPath [
-        xorg.libX11
-        xorg.libXcursor
-        xorg.libXrandr
-        xorg.libXi
-      ];
-    in
-    {
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    naersk,
+    pre-commit,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      naersk-lib = pkgs.callPackage naersk {};
+      libPath = with pkgs;
+        lib.makeLibraryPath [
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXrandr
+          xorg.libXi
+        ];
+    in {
       defaultPackage = naersk-lib.buildPackage ./.;
+
       devShell = with pkgs;
         mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+
           buildInputs = [
             cargo
             rustc
-            rustfmt
-            pre-commit
             rust-analyzer
-            rustPackages.clippy
           ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
           LD_LIBRARY_PATH = libPath;
         };
 
       formatter = pkgs.alejandra;
+      checks.pre-commit-check = pre-commit.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          alejandra.enable = true;
+          rustfmt.enable = true;
+          clippy.enable = true;
+          taplo.enable = true;
+        };
+      };
     });
 }
