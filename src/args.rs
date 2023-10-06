8244fe4b5753957e16a6c66a33ca2cf31d0f226c
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2023-10-06 17:26:57 +0300

    chore: add descriptions to CLI arguments

--- src/args.rs
+++ src/args.rs
@@ -6,12 +6,15 @@ pub struct Args {
     /// Path to a file containing a CHIP-8 program.
     pub program: std::path::PathBuf,
 
+    /// Window width
     #[arg(short = 'W', long, default_value_t = 640)]
     pub window_width: usize,
 
+    /// Window height
     #[arg(short = 'H', long, default_value_t = 320)]
     pub window_height: usize,
 
+    /// Speed of CPU (in CH8-ops/second).
     #[arg(short = 's', long, default_value_t = 700)]
     pub cpu_speed: u64,
 }
