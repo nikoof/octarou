5f01c0c3054ae5fc58e58c9a2a8fb5ecd38f6e65
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2023-10-14 17:35:30 +0300

    feat: add nix derivation for windows cross build

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
     pre-commit.url = "github:cachix/pre-commit-hooks.nix";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
@@ -12,9 +16,19 @@
     utils,
     naersk,
     pre-commit,
+    fenix,
   }:
     utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnsupportedSystem = true;
+      };
+      toolchain = with fenix.packages.${system};
+        combine [
+          minimal.cargo
+          minimal.rustc
+          targets.x86_64-pc-windows-gnu.latest.rust-std
+        ];
       naersk-lib = pkgs.callPackage naersk {};
       libPath = with pkgs;
         lib.makeLibraryPath [
@@ -51,6 +65,25 @@
           ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
           LD_LIBRARY_PATH = libPath;
+        };
+
+      packages.windowsCross =
+        (pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        })
+        .buildPackage
+        {
+          src = ./.;
+          pname = "chip8";
+
+          strictDeps = true;
+          depsBuildBuild = with pkgs.pkgsCross; [
+            mingwW64.stdenv.cc
+            mingwW64.windows.pthreads
+          ];
+
+          CARGO_BUILD_TARGET = "x86_64-pc-windows-gnu";
         };
 
       formatter = pkgs.alejandra;
