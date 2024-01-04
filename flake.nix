1f7d96705824d5e2c475df96935cbdaf43882438
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2024-01-04 14:45:15 +0200

    build: add texlab to paper devShell

--- flake.nix
+++ flake.nix
@@ -19,113 +19,115 @@
     fenix,
   } @ inputs:
     utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnsupportedSystem = true;
-      };
-      toolchain = with fenix.packages.${system};
-        combine [
-          minimal.cargo
-          minimal.rustc
-          targets.x86_64-pc-windows-gnu.latest.rust-std
-        ];
-      naersk-lib = pkgs.callPackage naersk {};
-      libPath = with pkgs;
-        lib.makeLibraryPath [
-          xorg.libX11
-          xorg.libXcursor
-          xorg.libXrandr
-          xorg.libXi
-        ];
-    in {
-      formatter = pkgs.alejandra;
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnsupportedSystem = true;
+        };
+        toolchain = with fenix.packages.${system};
+          combine [
+            minimal.cargo
+            minimal.rustc
+            targets.x86_64-pc-windows-gnu.latest.rust-std
+          ];
+        naersk-lib = pkgs.callPackage naersk {};
+        libPath = with pkgs;
+          lib.makeLibraryPath [
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+          ];
+      in rec
+      {
+        formatter = pkgs.alejandra;
 
-      defaultPackage = with pkgs;
-        naersk-lib.buildPackage rec {
+        defaultPackage = with pkgs;
+          naersk-lib.buildPackage rec {
+            src = ./.;
+            pname = "octarou";
+            nativeBuildInputs = [
+              makeWrapper
+            ];
+
+            postInstall = ''
+              wrapProgram "$out/bin/${pname}" --prefix LD_LIBRARY_PATH : "${libPath}"
+            '';
+
+            LD_LIBRARY_PATH = libPath;
+          };
+
+        packages.windowsCross =
+          (pkgs.callPackage naersk {
+            cargo = toolchain;
+            rustc = toolchain;
+          })
+          .buildPackage
+          {
+            src = ./.;
+            pname = "octarou";
+
+            strictDeps = true;
+            depsBuildBuild = with pkgs.pkgsCross; [
+              mingwW64.stdenv.cc
+              mingwW64.windows.pthreads
+            ];
+
+            CARGO_BUILD_TARGET = "x86_64-pc-windows-gnu";
+          };
+
+        devShell = with pkgs;
+          mkShell {
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+
+            buildInputs = [
+              cargo
+              rustc
+              rustfmt
+              rust-analyzer
+            ];
+
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+            LD_LIBRARY_PATH = libPath;
+          };
+
+        checks.pre-commit-check = pre-commit.lib.${system}.run {
           src = ./.;
-          pname = "octarou";
-          nativeBuildInputs = [
-            makeWrapper
-          ];
-
-          postInstall = ''
-            wrapProgram "$out/bin/${pname}" --prefix LD_LIBRARY_PATH : "${libPath}"
-          '';
-
-          LD_LIBRARY_PATH = libPath;
+          hooks = {
+            alejandra.enable = true;
+            rustfmt.enable = true;
+            # clippy.enable = true;
+            taplo.enable = true;
+            chktex.enable = true;
+            latexindent.enable = true;
+          };
         };
 
-      packages.windowsCross =
-        (pkgs.callPackage naersk {
-          cargo = toolchain;
-          rustc = toolchain;
-        })
-        .buildPackage
-        {
-          src = ./.;
-          pname = "octarou";
+        packages.paper = with pkgs;
+          stdenvNoCC.mkDerivation rec {
+            name = "ocatrou-paper";
+            src = ./doc/paper/src;
+            buildInputs = [coreutils texlive.combined.scheme-medium];
+            phases = ["unpackPhase" "buildPhase" "installPhase"];
 
-          strictDeps = true;
-          depsBuildBuild = with pkgs.pkgsCross; [
-            mingwW64.stdenv.cc
-            mingwW64.windows.pthreads
-          ];
+            buildPhase = ''
+              export PATH="${lib.makeBinPath buildInputs}"
+              mkdir -p .cache/texmf-var
+              env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+                latexmk -interaction=nonstopmode -pdf -lualatex \
+                ${src}/main.tex
+            '';
 
-          CARGO_BUILD_TARGET = "x86_64-pc-windows-gnu";
-        };
+            installPhase = ''
+              mkdir -p $out/doc
+              cp main.pdf $out/doc/paper.pdf
+            '';
+          };
 
-      devShell = with pkgs;
-        mkShell {
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
-
-          buildInputs = [
-            cargo
-            rustc
-            rustfmt
-            rust-analyzer
-          ];
-
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          LD_LIBRARY_PATH = libPath;
-        };
-
-      checks.pre-commit-check = pre-commit.lib.${system}.run {
-        src = ./.;
-        hooks = {
-          alejandra.enable = true;
-          rustfmt.enable = true;
-          # clippy.enable = true;
-          taplo.enable = true;
-          chktex.enable = true;
-          latexindent.enable = true;
-        };
-      };
-
-      packages.paper = with pkgs;
-        stdenvNoCC.mkDerivation rec {
-          name = "ocatrou-paper";
-          src = ./doc/paper/src;
-          buildInputs = [coreutils texlive.combined.scheme-medium];
-          phases = ["unpackPhase" "buildPhase" "installPhase"];
-
-          buildPhase = ''
-            export PATH="${lib.makeBinPath buildInputs}"
-            mkdir -p .cache/texmf-var
-            env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
-              latexmk -interaction=nonstopmode -pdf -lualatex \
-              ${src}/main.tex
-          '';
-
-          installPhase = ''
-            mkdir -p $out/doc
-            cp main.pdf $out/doc/
-          '';
-        };
-
-      devShells.paper = with pkgs;
-        mkShell {
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
-          packages = [texlive.combined.scheme-medium];
-        };
-    });
+        devShells.paper = with pkgs;
+          mkShell {
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+            inputsFrom = [packages.paper];
+            packages = [texlab];
+          };
+      });
 }
