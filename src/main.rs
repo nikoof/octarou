308d156a4f5cb6edffca7983e56a838ec7fbb6c7
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2024-01-04 19:12:52 +0200

    refactor: remove minifb and window mod

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@
 
 use anyhow::Result;
 use clap::Parser;
-use minifb::{ScaleMode, Window, WindowOptions};
 use std::io::Read;
 use std::{fs::File, path::Path};
 
@@ -12,25 +11,9 @@ use chip::{Chip8, Interpreter, Schip};
 
 pub mod args;
 pub mod chip;
-pub mod window;
 
 fn main() -> Result<()> {
     let args = Args::parse();
-
-    let window = Window::new(
-        match args.program.file_name() {
-            Some(s) => s.to_str().unwrap_or("CHIP-8"),
-            None => "CHIP-8",
-        },
-        args.window_width,
-        args.window_height,
-        WindowOptions {
-            resize: true,
-            scale_mode: ScaleMode::AspectRatioStretch,
-            ..WindowOptions::default()
-        },
-    )
-    .unwrap_or_else(|err| panic!("Failed to create window: {}", err));
 
     let program = read_program_from_file(args.program.as_path())?;
     let mut state: Box<dyn Interpreter> = match args.variant {
