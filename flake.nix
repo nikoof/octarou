11bad8d1d74b1ef67d5954aee4698c845d50e392
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2024-02-12 11:12:53 +0200

    fix(build): add alsa deps to flake.nix

--- flake.nix
+++ flake.nix
@@ -76,6 +76,10 @@
         // {
           inherit cargoArtifacts;
 
+          buildInputs = with pkgs;
+            [pkg-config]
+            ++ lib.optional (stdenv.isLinux) alsa-lib;
+
           LD_LIBRARY_PATH = libPath;
         });
 
@@ -121,6 +125,8 @@
 
       devShell = craneLib.devShell {
         inherit (self.checks.${system}.pre-commit-check) shellHook;
+
+        inputsFrom = [self.packages.${system}.default];
 
         packages = with pkgs; [
           trunk
